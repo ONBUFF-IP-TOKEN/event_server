ba32370e5fa3206a7cf1d6d833d0684f8712b4bc
byeongtaste <jwshin@onbuff.com>
2021-07-18 14:22:14 +0900

    [add] log 추가

--- rest_server/controllers/commonapi/api_event.go
+++ rest_server/controllers/commonapi/api_event.go
@@ -111,12 +111,14 @@ func PutEventSubmit(c echo.Context) error {
 			params.LastBalance = balance
 			if balance < itemInfo.MinAmountForSumbit {
 				// onit 이 최소 보유량보다 적으면 에러 리턴
+				log.Info("error : ", constant.ResultCodeText(constant.Result_NotEnoughTokenForSubmit), "  from:", params.WalletAddr)
 				resp.SetResult(constant.Result_NotEnoughTokenForSubmit)
 			} else {
 				// db 저장
 				if _, err := model.GetDB().PutEventSubmit(params); err != nil {
 					resp.SetResult(constant.Result_DBError)
 				} else {
+					log.Info("Submit success : ", params.WalletAddr, " count:", params.SubmitCnt, " amount:", balance)
 					resp.Success()
 					resp.Value = params
 				}
@@ -130,6 +132,7 @@ func PutEventSubmit(c echo.Context) error {
 				lastTime.Month() == curTime.Month() &&
 				lastTime.Day() == curTime.Day() {
 				//if curT < info.Ts+24*3600*1000 { // 응모하고 만 하루 지난것으로 체크할것인지 예비코드
+				log.Info("error : ", constant.ResultCodeText(constant.Result_ExistInfo), "  from:", params.WalletAddr)
 				resp.SetResult(constant.Result_ExistInfo)
 				resp.Value = info
 			} else {
@@ -141,18 +144,19 @@ func PutEventSubmit(c echo.Context) error {
 				params.LastBalance = balance
 				if balance < itemInfo.MinAmountForSumbit {
 					// onit 이 최소 보유량보다 적으면 에러 리턴
+					log.Info("error : ", constant.ResultCodeText(constant.Result_NotEnoughTokenForSubmit), "  from:", params.WalletAddr)
 					resp.SetResult(constant.Result_NotEnoughTokenForSubmit)
 				} else {
 					// db 저장
 					if _, err := model.GetDB().UpdateEventSubmit(params); err != nil {
 						resp.SetResult(constant.Result_DBError)
 					} else {
+						log.Info("Submit success : ", params.WalletAddr, " count:", params.SubmitCnt, " amount:", balance)
 						resp.Success()
 						resp.Value = params
 					}
 				}
 			}
-
 		}
 	}
 
